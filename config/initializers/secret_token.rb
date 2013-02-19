5c697f1206b59ee169b7780cedfc704ba60b7c06
jiapei <huang.gadela@gmail.com>
2013-02-19 19:59:55 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peijian::Application.config.secret_token = '4036c66de60d675b0f039d2faf90a6d7adc1218bfa90a82c8951f456ffebbf476394f3e54fe8339b4cf3cf6018c95f8449392dc245390dfccf534ce60a90f3ce'
